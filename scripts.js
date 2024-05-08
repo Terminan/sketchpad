b52fad5b8d45f394dc66f18c0e233b82bdcb485a
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-08 09:52:03 -0400

    Replace hexArray with convertToHex function
    Rather than relying on an array of predefined hex values,
    the convertToHex function allows the grid size to scale
    in its id assignment

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const hexArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+function convertToHex(num) {
+    return num.toString(16).padStart(2,"0").toUpperCase();
+}
 
 const mainContainer = document.querySelector(".mainContainer");
 
@@ -8,11 +10,11 @@ gridContainer.classList.add("gridContainer");
 for (let y = 0; y < 16; y++) {
     const gridRow = document.createElement("div");
     gridRow.classList.add("gridRows");
-    gridRow.setAttribute("id", `row${hexArray[y]}`);
+    gridRow.setAttribute("id", `row${convertToHex(y)}`);
     for (let x = 0; x < 16; x++) {
         const gridBlock = document.createElement("div");
         gridBlock.classList.add("gridBlocks");
-        gridBlock.setAttribute("id", `h${hexArray[x]}v${hexArray[y]}`);
+        gridBlock.setAttribute("id", `h${convertToHex(x)}v${convertToHex(y)}`);
         gridRow.appendChild(gridBlock);
     }
     gridContainer.appendChild(gridRow);
