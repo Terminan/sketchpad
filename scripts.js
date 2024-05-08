8113dc7f90b8cc630a58b889ff33c516720cb92e
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-08 07:58:09 -0400

    Create basic grid of divs with classes and ids

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const hexArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+
+const mainContainer = document.querySelector(".mainContainer");
+
+for (let y = 0; y < 16; y++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("gridRows");
+    gridRow.setAttribute("id", `row${hexArray[y]}`);
+    for (let x = 0; x < 16; x++) {
+        const gridBlock = document.createElement("div");
+        gridBlock.classList.add("gridBlocks");
+        gridBlock.setAttribute("id", `h${hexArray[x]}v${hexArray[y]}`);
+        gridRow.appendChild(gridBlock);
+    }
+    mainContainer.appendChild(gridRow);
+}
